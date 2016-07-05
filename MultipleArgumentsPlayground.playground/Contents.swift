6219fddb7d6f82984190b1704c7b5f071d400f1e
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-05 14:44:31 -0400

    Multiple arguments in Swift is interesting

---
+++ MultipleArgumentsPlayground.playground/Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+
+func moonsOfPlanet (planet: String, numberOfMoons: Int) {
+    
+    print("There are \(numberOfMoons) moons orbiting \(planet).")
+    
+}
+
+var planet = "Jupiter"
+var moons = 67
+
+moonsOfPlanet(planet, numberOfMoons: moons)
+
+planet = "Mars"
+moons = 2
+moonsOfPlanet(planet, numberOfMoons: moons)
+
+planet = "Earth"
+moons = 1
+moonsOfPlanet(planet, numberOfMoons: moons)
+
+planet = "Uranus"
+moons = 27
+moonsOfPlanet(planet, numberOfMoons: moons)
+
+planet = "Saturn"
+moons = 62
+moonsOfPlanet(planet, numberOfMoons: moons)
+
+planet = "Pluto"
+moons = 5
+moonsOfPlanet(planet, numberOfMoons: moons)
+
+planet = "Mercury"
+moons = 0
+moonsOfPlanet(planet, numberOfMoons: moons)
+
+planet = "Venus"
+moons = 0
+moonsOfPlanet(planet, numberOfMoons: moons)
+
+planet = "Neptune"
+moons = 14
+moonsOfPlanet(planet, numberOfMoons: moons)
